59b0c2ba954e901b32b3b56b361a861df16e7338
Tanguy Boisset <tanguy.boisset@gmail.com>
2021-09-08 14:37:32 +0200

    change terminal font and empty user input behavior

--- src/home/Commands.js
+++ src/home/Commands.js
@@ -1,8 +1,13 @@
 function Commands(input){
     switch (input){
+
         case "help":
             return "Here's the list of all commands :" + "<br/>" +"\
             &nbsp;&nbsp;- help : display the available commands"
+
+        case "":
+            return ""
+        
         default:
             return "Command '" + input + "' not found"
     }
