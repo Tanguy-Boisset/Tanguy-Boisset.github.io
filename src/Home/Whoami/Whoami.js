24af2d29a4876a9e74b4be5c073ccc93fd113d86
Tanguy-Boisset <tanguy.boisset@gmail.com>
2022-04-28 00:17:45 +0200

    whoami done

--- src/Home/Whoami/Whoami.js
+++ src/Home/Whoami/Whoami.js
@@ -3,7 +3,16 @@ import './Whoami.css'
 function Whoami() {
     return (
       <div className="Whoami">
-        <p>HELLO !!!!!!</p>
+        <h1>Whoami</h1>
+        <div className='mainPrez'>
+            <p>
+                My name is Tanguy 'Sopalinge' Boisset.<br/><br/>
+                I'm a cybersecurity student at CentraleSupelec and currently doing an internship at Mazars as cybersecurity consultant.<br/><br/>
+                I'm interested in computer science, especially cybersecurity, and a CTF player.<br/><br/>
+                I'm also a big fan of e-sport and a golf player.
+            </p>
+            <img src={require('./baguette-pp.jpg')} className="whoamiImg"/>
+        </div>
       </div>
     );
   }
