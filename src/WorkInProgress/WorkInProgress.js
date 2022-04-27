35645058886a462e0d8390b8ea94a5b2f7bd5868
Tanguy-Boisset <tanguy.boisset@gmail.com>
2022-04-27 23:22:49 +0200

    wip done

--- src/WorkInProgress/WorkInProgress.js
+++ src/WorkInProgress/WorkInProgress.js
@@ -3,6 +3,7 @@ import './WorkInProgress.css'
 function WorkInProgress() {
     return (
       <div className="WorkInProgress">
+        <img src={require('./wip.png')} className="WIPimg"/>
         <p>This page is not finished yet ! Come back later !</p>
       </div>
     );
